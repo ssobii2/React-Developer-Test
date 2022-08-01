336ef4e7d98c9b2176c3bca63141f6155577026b
THUNDER-PC\subha <rntvtf@pte.hu>
2022-08-01 20:20:27 +0200

    fixed mini-cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,7 @@ import ProductImg from "../images/product.svg";
 
 export default class Navbar extends Component {
   container = React.createRef();
+  container_2 = React.createRef();
 
   handleClick = () => {
     this.setState((state) => {
@@ -16,6 +17,14 @@ export default class Navbar extends Component {
       };
     });
   };
+
+  handleClickCart = () => {
+    this.setState((state) => {
+      return {
+        cart: !state.cart,
+      };
+    });
+  }
 
   handleClickOutside = (event) => {
     if (
@@ -28,23 +37,38 @@ export default class Navbar extends Component {
     }
   };
 
+
+  handleClickOutsideCart = (event) => {
+    if (
+      this.container_2.current &&
+      !this.container_2.current.contains(event.target)
+    ) {
+      this.setState({
+        cart: false,
+      });
+    }
+  }
+
   componentDidMount() {
     document.addEventListener("mousedown", this.handleClickOutside);
+    document.addEventListener("mousedown", this.handleClickOutsideCart);
   }
   componentWillUnmount() {
     document.removeEventListener("mousedown", this.handleClickOutside);
+    document.removeEventListener("mousedown", this.handleClickOutsideCart);
   }
 
   constructor(props) {
     super(props);
     this.state = {
       open: false,
+      cart: false,
     };
   }
 
   render() {
     return (
-      <nav className="nav-bar" ref={this.container}>
+      <nav className="nav-bar">
         <div className="nav-items">
           <NavLink
             to="/"
@@ -69,7 +93,7 @@ export default class Navbar extends Component {
           <img src={Logo} alt="logo" width="41px" height="41px" />
         </div>
         <div className="actions">
-          <div className="dropdown">
+          <div className="dropdown" ref={this.container}>
             <div className="dropdown-button">
               <img src={Dollar} alt="dollar" onClick={this.handleClick} />
               {this.state.open ? (
@@ -97,64 +121,77 @@ export default class Navbar extends Component {
           </div>
 
           <div className="dropdown-2">
-            <div id="overlay"></div>
-            <img className="cart" src={Cart} alt="cart" />
-            <div className="badge">3</div>
-            <div className="dropdown-content-2">
-              <p className="margin-p">
-                <b>My Bag</b>,3 items
-              </p>
-              <div className="cart-product">
-                <div className="info-width">
-                  <h3>Apollo</h3>
-                  <p>Running Short</p>
-                  <h3 className="mb">$50.00</h3>
-                  <h4 className="mb">SIZE:</h4>
-                  <button className="size-btn sz-btn">XS</button>
-                  <button className="size-btn sz-btn">S</button>
-                  <button className="size-btn sz-btn">M</button>
-                  <button className="size-btn sz-btn">L</button>
-                  <h4 className="mb">COLOR:</h4>
-                  <div className="boxes boxes-2">
-                    <div className="gray"></div>
-                    <div className="black"></div>
-                    <div className="green"></div>
+            <img
+              className="cart"
+              src={Cart}
+              alt="cart"
+              onClick={this.handleClickCart}
+            />
+            <div className="badge" ref={this.container_2}>3</div>
+            {this.state.cart && (
+              <>
+                <div id="overlay"></div>
+                <div className="dropdown-content-2">
+                  <p className="margin-p">
+                    <b>My Bag</b>,3 items
+                  </p>
+                  <div className="cart-product">
+                    <div className="info-width">
+                      <p>Apollo</p>
+                      <p>Running Short</p>
+                      <h3 className="mb">$50.00</h3>
+                      <p className="mb">Size:</p>
+                      <button className="size-btn sz-btn">XS</button>
+                      <button className="size-btn sz-btn">S</button>
+                      <button className="size-btn sz-btn">M</button>
+                      <button className="size-btn sz-btn">L</button>
+                      <p className="mb">Color:</p>
+                      <div className="boxes boxes-2">
+                        <div className="gray"></div>
+                        <div className="black"></div>
+                        <div className="green"></div>
+                      </div>
+                    </div>
+                    <div className="mp-btn">
+                      <div className="m-p-btn">
+                        <button style={{ width: "24px", height: "24px" }}>
+                          +
+                        </button>
+                        <p>1</p>
+                        <button style={{ width: "24px", height: "24px" }}>
+                          -
+                        </button>
+                      </div>
+                      <img
+                        src={ProductImg}
+                        alt="product"
+                        width="221px"
+                        height="190px"
+                      />
+                    </div>
+                  </div>
+                  <div className="mini-cart">
+                    <b className="margin-r">Total</b>
+                    <b>$200.00</b>
+                    <div>
+                      <Link to="/cart">
+                        <button className="bag-btn">View Bag</button>
+                      </Link>
+                      <button
+                        className="cart-btn"
+                        style={{
+                          width: "140px",
+                          height: "43px",
+                          marginRight: "25px",
+                        }}
+                      >
+                        Checkout
+                      </button>
+                    </div>
                   </div>
                 </div>
-                <div className="mp-btn">
-                  <div className="m-p-btn">
-                    <button style={{ width: "24px", height: "24px" }}>+</button>
-                    <p>1</p>
-                    <button style={{ width: "24px", height: "24px" }}>-</button>
-                  </div>
-                  <img
-                    src={ProductImg}
-                    alt="product"
-                    width="221px"
-                    height="190px"
-                  />
-                </div>
-              </div>
-              <div className="mini-cart">
-                <b className="margin-r">Total</b>
-                <b>$200.00</b>
-                <div>
-                  <Link to="/cart">
-                    <button className="bag-btn">View Bag</button>
-                  </Link>
-                  <button
-                    className="cart-btn"
-                    style={{
-                      width: "140px",
-                      height: "43px",
-                      marginRight: "25px",
-                    }}
-                  >
-                    Checkout
-                  </button>
-                </div>
-              </div>
-            </div>
+              </>
+            )}
           </div>
         </div>
       </nav>
